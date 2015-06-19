cdae8c7eaed43c41336d426a526d0808031813b8
Emilio Murta <emilio.murta@msitbrasil.com>
2015-06-19 10:21:50 -0300

    add cors configuration (cookie based)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token, if: :json_request?
+  skip_before_action :verify_authenticity_token, if: :valid_cookies_sent?
+  skip_before_action :verify_authenticity_token, :only => [:options]
 
   before_action :authenticate_user!
-
-  before_action :authenticate_cors_user
-  before_action :cors_set_access_control_headers
-  skip_before_action :verify_authenticity_token, :only => [:options]
   skip_before_action :authenticate_user!, :only => [:options]
+
+  before_action :cors_set_access_control_headers
 
   respond_to :json
 
@@ -26,22 +25,15 @@ class ApplicationController < ActionController::Base
       'api-lafavoritta-easypos.herokuapp.com' => 'https://lafavoritta-easypos.herokuapp.com'
   }
 
-  def authenticate_cors_user
-    if request.xhr? && !user_signed_in?
-      error = { :error => 'You must be logged in.' }
-      render :json => error, :status => 401
-    end
-  end
-
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = ALLOWED_CLIENTS[env['HTTP_HOST']]
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS, PATCH'
-    headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-XSRF-TOKEN, Content-Type'
+    headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, Content-Type'
     headers['Access-Control-Allow-Credentials'] = 'true'
     headers['Access-Control-Max-Age'] = '1728000'
   end
 
-  def json_request?
-    request.format.json?
+  def valid_cookies_sent?
+    valid_authenticity_token?(session, cookies['XSRF-TOKEN'])
   end
 end
