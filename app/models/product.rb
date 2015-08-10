dcbf2d9a7c8e72ba51316776e1be6112030100cd
Emilio Murta <emilio.murta@msitbrasil.com>
2015-08-10 17:55:32 -0300

    validates uniqueness of barcode on product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, :barcode, presence: true, uniqueness: { case_sensitive: false }
   validates :price, presence: true
 
   has_many :storage_entries
