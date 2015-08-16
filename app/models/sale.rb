d33fd6956b4d6ae87e3261dc9edcc118d4e9e543
Lucas Angelino <lucas.angelino@gmail.com>
2015-08-15 21:48:07 -0300

    Fix association between sale and saller(user)

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,5 @@
 class Sale < ActiveRecord::Base
-  belongs_to :saller, class_name: 'User'
+  belongs_to :saller, class_name: 'User', foreign_key: 'user_id'
   belongs_to :point_of_sale
   has_many :sale_entries, dependent: :destroy
 end
