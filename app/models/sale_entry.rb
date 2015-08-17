0a20d3a118ab847848370512f953459cd112d4a0
Lucas Angelino <lucas.angelino@gmail.com>
2015-08-17 00:05:04 -0300

    Add validations to models

--- app/models/sale_entry.rb
+++ app/models/sale_entry.rb
@@ -1,4 +1,9 @@
 class SaleEntry < ActiveRecord::Base
+  validates :sale, presence: true
+  validates :storage_entry, presence: true
+  validates :price_at_date, presence: true
+  validates :quantity, presence: true
+
   belongs_to :sale
   belongs_to :storage_entry, dependent: :destroy
   has_one :product, through: :storage_entry
